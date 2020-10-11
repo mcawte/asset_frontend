8c2ae2ba0edd1e3397957d35d110f1852a7a1abc
mcawte <mcawte@gmail.com>
2020-10-11 14:52:05 +1300

    Push build for ease of use

---
+++ build/precache-manifest.6e21a9d939fa159b4a0886cf07c00158.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231fb9dd301e341be48364a026274bd7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e33ea9ff8b7c10cb97d9",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9c529a937dfe65d701e4",
+    "url": "./static/js/2.57f4f0b7.chunk.js"
+  },
+  {
+    "revision": "14dc907096ff79e6b7be55307d0260dd",
+    "url": "./static/js/2.57f4f0b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e33ea9ff8b7c10cb97d9",
+    "url": "./static/js/main.b15d1dcb.chunk.js"
+  },
+  {
+    "revision": "e5debd517f6745697500",
+    "url": "./static/js/runtime-main.337e0502.js"
+  }
+]);
